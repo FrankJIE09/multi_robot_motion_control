06be3bc215c0b7497c643d3a18bf8e92aa8e6882
sage <247625515@qq.com>
2025-02-24 21:51:06 +0800

    update

--- elibot/CPS.py
+++ elibot/CPS.py
@@ -67,7 +67,8 @@ class CPSClient:
 
     def moveByJoint(self, target_joint, speed=10, block=True):
         if target_joint[0] <= 120:
-            raise Exception("Joint1 over limit!.")
+            print(target_joint[0])
+            raise Exception("Joint1 over limit!",target_joint[0])
         suc, result, _ = self.sendCMD("moveByJoint", {"targetPos": target_joint, "speed": speed})
         if suc:
             print(f"Move command sent: Target position {target_joint} with speed {speed}")
@@ -258,7 +259,7 @@ def desire_right_pose(rpy_array=None):
 
 
 if __name__ == "__main__":
-    robot_ip = "192.168.188.200"
+    robot_ip = "192.168.188.201"
     controller = CPSClient(robot_ip)
 
     if controller.connect():
@@ -268,11 +269,12 @@ if __name__ == "__main__":
         print("pos :", formatted_tcp_pos)
         formatted_joint_pos = [round(pos, 2) for pos in controller.getJointPos()]
         print("joint :", formatted_joint_pos)
+        controller.moveByJoint_right([-201.58, -120.88, -114.04, -50.78, -71.61, -116.05])
 
-        # controller.connect_gripper()
-        # controller.run_gripper(0)
-        # time.sleep(3)
-        # controller.run_gripper(255,force=255,speed=255)
+        controller.connect_gripper()
+        controller.run_gripper(0)
+        time.sleep(3)
+        controller.run_gripper(255,force=255,speed=255)
         #
         # _, error_state, move_state, current_position = controller.read_gripper_state()
         # print("当前位置：", current_position)
@@ -281,9 +283,9 @@ if __name__ == "__main__":
         # rpy_angles = desire_left_pose(rpy_array=[180,0,180]) # left 垂直向下
         # pose[3:6] = rpy_angles
         # controller.move_robot(pose)
-        rpy_angles = desire_left_pose(rpy_array=[0, -90,0])  # left 水平向前
-        pose[3:6] = rpy_angles
-        controller.move_robot(pose)
+        # rpy_angles = desire_left_pose(rpy_array=[0, -90,0])  # left 水平向前
+        # pose[3:6] = rpy_angles
+        # controller.move_robot(pose)
 
         # rpy_angles = desire_left_pose(rpy_array=[-90,0,90]) # left 水平向前 相机朝右
         # pose[3:6] = rpy_angles
